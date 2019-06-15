c19528d41004d7ae8def49113cfdee78be223d2a
Mitja <mitjagalkin@gmail.com>
2019-06-15 22:53:17 +0200

    Add backButton

--- Frontend/eclipseWorkspace/Frontend/src/main/java/frontend/Controller.java
+++ Frontend/eclipseWorkspace/Frontend/src/main/java/frontend/Controller.java
@@ -1,14 +1,7 @@
 package frontend;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.LinkedList;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -18,42 +11,82 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToolBar;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 
 public class Controller {
 	
-	   @FXML
-	    private ListView<String> lawListView;
-	   
+		@FXML
+		private TextField searchTextField;
+
+		@FXML
+		private Label titleLabel;
+	
+		@FXML
+    	private Button searchButton;
+		
+		@FXML
+    	private Button backButton;
+
+		@FXML
+		private Pane placeholderPane;
+
+		@FXML
+    	private Button checkButton;
+
+		@FXML
+		private Button deleteButton;
+		
 	    @FXML
-	    private TextField searchTextField;
+	    private StackPane listViewStackPane;
 
 	    @FXML
-	    private BorderPane root;
+	    private StackPane referStackPane;
 	    
 	    @FXML
-	    private ToolBar toolBar;
-	   
-	    @FXML
-	    private HBox placeholderHBox;
-	    
-	    @FXML
-	    private Button searchButton;
-	    
-	    @FXML
-	    private Button deleteButton;
+	    private StackPane statuesStackPane;
 
-	    @FXML
-	    private Button checkButton;
+    	@FXML
+    	private ListView<String> statuesListView;
 
+    	@FXML
+    	private ListView<String> paragraphsListView;
+    
+    	@FXML
+    	private ListView<String> referListView; // Vorübergehend
+
+    	private int listViewStatus = 0;
+    	private Root r;
+    	
+    	@FXML
+        void backButtonAction(MouseEvent event) {
+    		if(listViewStatus == 1) {
+    			listViewStatus = 0;
+    			statuesStackPane.toFront();
+    			statuesListView.setDisable(false);
+    			
+    			paragraphsListView.toBack();
+    			paragraphsListView.setDisable(true);
+    			
+    			titleLabel.setText("Gesetzbücher");
+    			backButton.setDisable(true);
+    			
+    		} else if(listViewStatus == 2) {
+    			listViewStatus = 1;
+    			paragraphsListView.toFront();
+    			paragraphsListView.setDisable(false);
+    			
+    			statuesStackPane.toBack();
+    			statuesListView.setDisable(true);
+    		}
+        }
+    	
 	    @FXML
 	    void checkButtonAction(MouseEvent event) {
 	    	
@@ -65,85 +98,213 @@ public class Controller {
 	    }
 	    
 	    @FXML
-	    void lawListViewAction(MouseEvent event) {
-	    	if(lawListView.getSelectionModel().getSelectedItem() != null) {
+	    void statuesListViewAction(MouseEvent event) {
+	    	if(statuesListView.getSelectionModel().getSelectedItem() != null) {
 	    		checkButton.setDisable(false);
+	    		if(event.getButton().equals(MouseButton.PRIMARY)) {
+	    			if(event.getClickCount() == 2) {
+	    				listViewStatus = 1;
+	    				backButton.setDisable(false);
+	    				
+	    				paragraphsListView.toFront();
+	        			paragraphsListView.setDisable(false);
+	    				
+	    				ObservableList<String> paragraphItems = FXCollections.observableArrayList();
+	    				
+	    				paragraphItems = getParagraphs();
+	    				
+	    				paragraphsListView.setItems(paragraphItems);
+	    				paragraphsListView.setVisible(true);
+	    				
+	    				statuesListView.setDisable(true);
+	    				statuesListView.toBack();
+	    				statuesStackPane.toBack();
+	    			}
+	    		}
+	    	}
+	    }
+	    
+	    @FXML
+	    void paragraphsListViewsubparagraphsListView(MouseEvent event) {
+
+	    }
+	    
+	    @FXML
+	    void referListViewAction(MouseEvent event) {
+
+	    }
+	    
+	    @FXML
+	    void searchButtonAction(MouseEvent event) {
+	    	if(listViewStatus == 0) {
+	    		int lawSize = statuesListView.getItems().size();
+		    	MultipleSelectionModel<String> model = statuesListView.getSelectionModel();
+		    	
+		    	LinkedList<String> getMatches = new LinkedList<String>();
+
+		    	if(!searchTextField.getText().isEmpty() || searchTextField.getText() != null) {
+
+		    		getMatches.clear();
+		    		for(int i = 0; i < lawSize; i++) {
+		    			if(statuesListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+		    				getMatches.add(statuesListView.getItems().get(i));
+		    			}
+		    		}
+		    		
+		    		if(model.getSelectedItem() != null && model.getSelectedIndex() != getMatches.size()-1) {
+
+		    			for(int i = model.getSelectedIndex()+1; i < statuesListView.getItems().size(); i++) {
+			    			if(statuesListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+			    				model.select(i);
+			    				statuesListView.scrollTo(i);
+			    				break;
+			    			}
+			    		}	
+	    			} else {
+
+	    				for(int i = 0; i < lawSize; i++) {
+	    	    			if(statuesListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+	    	    				model.select(i);
+	    	    				statuesListView.scrollTo(i);
+	    	    				break;
+	    	    			}
+	    	    		}
+	    			}
+		    	}
+	    	} else if(listViewStatus == 1) {
+	    		int pargraphsSize = paragraphsListView.getItems().size();
+		    	MultipleSelectionModel<String> model = paragraphsListView.getSelectionModel();
+		    	
+		    	LinkedList<String> getMatches = new LinkedList<String>();
+
+		    	if(!searchTextField.getText().isEmpty() || searchTextField.getText() != null) {
+
+		    		getMatches.clear();
+		    		for(int i = 0; i < pargraphsSize; i++) {
+		    			if(paragraphsListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+		    				getMatches.add(paragraphsListView.getItems().get(i));
+		    			}
+		    		}
+		    		
+		    		if(model.getSelectedItem() != null && model.getSelectedIndex() != getMatches.size()-1) {
+
+		    			for(int i = model.getSelectedIndex()+1; i < paragraphsListView.getItems().size(); i++) {
+			    			if(paragraphsListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+			    				model.select(i);
+			    				paragraphsListView.scrollTo(i);
+			    				break;
+			    			}
+			    		}	
+	    			} else {
+
+	    				for(int i = 0; i < pargraphsSize; i++) {
+	    	    			if(paragraphsListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+	    	    				model.select(i);
+	    	    				paragraphsListView.scrollTo(i);
+	    	    				break;
+	    	    			}
+	    	    		}
+	    			}
+		    	}
+	    	} else if(listViewStatus == 2) {
+	    		int referSize = referListView.getItems().size();
+		    	MultipleSelectionModel<String> model = referListView.getSelectionModel();
+		    	
+		    	LinkedList<String> getMatches = new LinkedList<String>();
+
+		    	if(!searchTextField.getText().isEmpty() || searchTextField.getText() != null) {
+
+		    		getMatches.clear();
+		    		for(int i = 0; i < referSize; i++) {
+		    			if(referListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+		    				getMatches.add(referListView.getItems().get(i));
+		    			}
+		    		}
+		    		
+		    		if(model.getSelectedItem() != null && model.getSelectedIndex() != getMatches.size()-1) {
+
+		    			for(int i = model.getSelectedIndex()+1; i < referListView.getItems().size(); i++) {
+			    			if(referListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+			    				model.select(i);
+			    				referListView.scrollTo(i);
+			    				break;
+			    			}
+			    		}	
+	    			} else {
+
+	    				for(int i = 0; i < referSize; i++) {
+	    	    			if(referListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
+	    	    				model.select(i);
+	    	    				referListView.scrollTo(i);
+	    	    				break;
+	    	    			}
+	    	    		}
+	    			}
+		    	}
 	    	}
 	    }
 
-	    @FXML
-	    void searchButtonAction(MouseEvent event) {
-	    	int lawSize = lawListView.getItems().size();
-	    	MultipleSelectionModel<String> model = lawListView.getSelectionModel();
-	    	
-	    	LinkedList<String> getMatches = new LinkedList<String>();
-	    	
-	    	if(!searchTextField.getText().isEmpty() || searchTextField.getText() != null) {
-	    		getMatches.clear();
-	    		for(int i = 0; i < lawSize; i++) {
-	    			if(lawListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
-	    				getMatches.add(lawListView.getItems().get(i));
-	    			}
-	    		}
-	    		
-	    		if(model.getSelectedItem() != null && model.getSelectedIndex() != getMatches.size()-1) {
-	    			
-	    			for(int i = model.getSelectedIndex()+1; i < lawListView.getItems().size(); i++) {
-		    			if(lawListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
-		    				model.select(i);
-		    				lawListView.scrollTo(i);
-		    				break;
-		    			}
-		    		}	
-    			} else {
-    				for(int i = 0; i < lawSize; i++) {
-    	    			if(lawListView.getItems().get(i).toLowerCase().contains(searchTextField.getText().toLowerCase())) {
-    	    				model.select(i);
-    	    				lawListView.scrollTo(i);
-    	    				break;
-    	    			}
-    	    		}
-    			}
-	    	}
-	    }
 
 	    @FXML
 	    void initialize() {
-	        assert root != null : "fx:id=\"root\" was not injected: check your FXML file 'View.fxml'.";
-	        assert lawListView != null : "fx:id=\"lawListView\" was not injected: check your FXML file 'View.fxml'.";
-	        assert toolBar != null : "fx:id=\"toolBar\" was not injected: check your FXML file 'View.fxml'.";
-	        assert searchTextField != null : "fx:id=\"searchTextField\" was not injected: check your FXML file 'View.fxml'.";
-	        assert searchButton != null : "fx:id=\"searchButton\" was not injected: check your FXML file 'View.fxml'.";
-	        assert placeholderHBox != null : "fx:id=\"placeholderHBox\" was not injected: check your FXML file 'View.fxml'.";
-	        assert checkButton != null : "fx:id=\"checkButton\" was not injected: check your FXML file 'View.fxml'.";
-	        assert deleteButton != null : "fx:id=\"deleteButton\" was not injected: check your FXML file 'View.fxml'.";
+	        assert backButton != null : "fx:id=\"backButton\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert searchTextField != null : "fx:id=\"searchTextField\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert searchButton != null : "fx:id=\"searchButton\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert placeholderPane != null : "fx:id=\"placeholderPane\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert checkButton != null : "fx:id=\"checkButton\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert deleteButton != null : "fx:id=\"deleteButton\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert titleLabel != null : "fx:id=\"titleLabel\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert listViewStackPane != null : "fx:id=\"listViewStackPane\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert referStackPane != null : "fx:id=\"referStackPane\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert referListView != null : "fx:id=\"referListView\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert paragraphsListView != null : "fx:id=\"paragraphsListView\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert statuesStackPane != null : "fx:id=\"listViewInnerStackPane\" was not injected: check your FXML file 'OldView.fxml'.";
+	        assert statuesListView != null : "fx:id=\"statuesListView\" was not injected: check your FXML file 'OldView.fxml'.";
 	        
 	        setItems();
 	    }
 	    
 	    private void setItems() {
-	    	 
-
 	    	 try {
-	    		 ObservableList<String> items = FXCollections.observableArrayList();
+	    		 ObservableList<String> statuesItems = FXCollections.observableArrayList();
 		    	 
 		    	 ObjectMapper mapper = new ObjectMapper();
-		    	 Root r = mapper.readValue(new File("/Users/Mitja/Desktop/test.json"), Root.class);
+		    	 mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		    	 r = mapper.readValue(new File("/Users/Mitja/Desktop/root.json"), Root.class);
 		    	 
 		    	 String item;
 		    	 
 		    	 for(int i = 0; i < r.getStatues().length; i++) {
 		    		 item = "";
 		    		 item += r.getStatues()[i].getFullname() + " (" + r.getStatues()[i].getShorthand() + ")";
-		    		 for(int j = 0; j < r.getStatues()[i].getParagraphs().length; j++) {
-		    			 item += " Paragraph: " + r.getStatues()[i].getParagraphs()[j].getNumber();
-		    		 }
-		    		 items.add(item);
+		    		 statuesItems.add(item);
 		    	 }
-		    	 
-		    	 lawListView.setItems(items);
+		    	 statuesListView.setItems(statuesItems);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+	    }
+	    
+	    private ObservableList<String> getParagraphs() {
+	    	ObservableList<String> paragraphItems = FXCollections.observableArrayList();
+	    	String item = "";
+			
+			for(int i = 0; i < r.getStatues().length; i++) {
+				if(statuesListView.getSelectionModel().getSelectedItem().contains(r.getStatues()[i].getFullname())) {
+					titleLabel.setText(r.getStatues()[i].getShorthand() + " - Paragraphen");	
+					for(int k = 0; k < r.getStatues()[i].getParagraphs().length; k++) {
+    					item = "";
+    					item += r.getStatues()[i].getParagraphs()[k].getNumber();
+    					paragraphItems.add(item);
+    				}
+				}
+			}
+			
+			if(item.isEmpty()) {
+				paragraphItems.add("Keine Einträge vorhanden!");
+			} 
+			
+			return paragraphItems;
 	    }
 }
