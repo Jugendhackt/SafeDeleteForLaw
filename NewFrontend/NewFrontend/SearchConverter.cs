2d649d37eab79a1617629aff446d4919654b8c78
Tobias Brohl <tobrohl@gmail.com>
2019-08-20 12:44:01 +0200

    SK

--- NewFrontend/NewFrontend/SearchConverter.cs
+++ NewFrontend/NewFrontend/SearchConverter.cs
@@ -9,9 +9,9 @@ using DataStructures;
 namespace NewFrontend {
 public class SearchConverter : IMultiValueConverter {
 	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
-		IEnumerable<Statue> objects = (List<Statue>) values[0];
+		IEnumerable objects = (IEnumerable) values[0];
 		String s = (string) values[1];
-		return objects.Where(x => x.ToString().Contains(s)).Select(x=>x.ToString());
+		return objects.Cast<object>() .Where(x => x.ToString().Contains(s)).Select(x=>x.ToString());
 	}
 
 	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
