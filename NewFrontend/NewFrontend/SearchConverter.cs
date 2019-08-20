f1aaf70314f62a82b396f99ec3a6e1f576da9342
Tobias Brohl <tobrohl@gmail.com>
2019-08-20 19:14:58 +0200

    imps

--- NewFrontend/NewFrontend/SearchConverter.cs
+++ NewFrontend/NewFrontend/SearchConverter.cs
@@ -11,12 +11,17 @@ public class SearchConverter : IMultiValueConverter {
 	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
 		if (values[0] is IEnumerable objects) {
 			if (!objects.Cast<object>().Any()) {
-				return "Keine Einträge";
+				return new[] {"Keine Einträge"};
 			}
+
 			String s = (string) values[1];
-			return objects.Cast<object>() .Where(x => x.ToString().Contains(s));
+			return objects.Cast<object>().Where(x => x.ToString().Contains(s));
 
 		}
+		else {
+			return new[] {"Fehler"};
+		}
+
 	}
 
 	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
